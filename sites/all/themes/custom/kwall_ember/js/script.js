da721f37e595504ef337444cf53e7e21449d89a1
Abel Munoz <jabelmunoz@gmail.com>
2017-02-24 16:38:05 +0000

    update with sermons content type

---
+++ sites/all/themes/custom/kwall_ember/js/script.js
@@ -0,0 +1,16 @@
+
+jQuery(document).ready(function($) {
+
+// Place your code here.
+
+
+  $(".node-type-person #person-node-form input#edit-field-person-name-und-0-given").prop('readonly', true);
+  $(".node-type-person #person-node-form input#edit-field-person-name-und-0-family").prop('readonly', true);
+  $(".node-type-person #person-node-form input#edit-title-field-und-0-value").prop('readonly', true);
+  $(".node-type-person #person-node-form input#edit-field-position-und-0-value").prop('readonly', true);
+  $(".node-type-person #person-node-form input#edit-field-telephone-und-0-value").prop('readonly',true);
+  $(".node-type-person #person-node-form input#edit-field-fax-und-0-value").prop('readonly',true);
+  $(".node-type-person #person-node-form input#edit-field-e-mail-und-0-email").prop('readonly',true);
+  $(".node-type-person #person-node-form input#edit-field-campus-und-0-target-id").prop('readonly',true);
+  $(".node-type-person #person-node-form input#edit-field-department-tax-und-0-target-id").prop('readonly',true)
+});
